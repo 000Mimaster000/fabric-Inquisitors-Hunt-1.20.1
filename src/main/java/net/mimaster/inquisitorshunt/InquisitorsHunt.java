e1a3381418a2992afa4d447219bbed07b6da3ce5
Arseniy <akemov234@gmail.com>
2024-04-14 19:29:30 +0300

    мда

--- src/main/java/net/mimaster/inquisitorshunt/InquisitorsHunt.java
+++ src/main/java/net/mimaster/inquisitorshunt/InquisitorsHunt.java
@@ -11,6 +11,6 @@ public class InquisitorsHunt implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Minecraft world!");
 	}
 }
