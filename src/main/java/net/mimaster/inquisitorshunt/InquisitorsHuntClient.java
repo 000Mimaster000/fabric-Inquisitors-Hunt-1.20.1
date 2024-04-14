ae5d2022f5d87abad2a4aacc6789d252d9c8e34a
Arseniy <akemov234@gmail.com>
2024-04-14 18:57:59 +0300

    Initial commit

---
+++ src/main/java/net/mimaster/inquisitorshunt/InquisitorsHuntClient.java
@@ -0,0 +1,10 @@
+package net.mimaster.inquisitorshunt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InquisitorsHuntClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mimaster/inquisitorshunt/InquisitorsHuntDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mimaster.inquisitorshunt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InquisitorsHuntDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
